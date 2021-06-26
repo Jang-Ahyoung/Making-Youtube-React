edfcd67f71917a0e4f9729cdc781d8fe2d6cf371
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-06-26 10:16:50 +0900

    header 우측 센션 생성

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,9 +1,12 @@
 import styles from './search_header.module.css';
 import React, { memo, useRef } from 'react';
-
+import VideoCallIcon from '@material-ui/icons/VideoCall';
+import AppsIcon from '@material-ui/icons/Apps';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import Skeleton from '../skeleton/skeleton';
 //memo는 전달되는 props 변경되지 않으면 리렌더 되지않고 props 바뀌면 다시 렌더링 되는 아이
-// 즉 onSearch 계속 새로운 것을 ㅗ바뀌면 렌더 계속 발생할수있다는 이야기! -> app으로가
-const SearchHeader = memo(({ onSearch }) => {
+// 즉 onSearch 계속 새로운 것을 바뀌면 렌더 계속 발생할수있다는 이야기! -> app으로가
+const SearchHeader = memo(({ onSearch, isLoading }) => {
   const inputRef = useRef();
   const handleSearch = () => {
     const value = inputRef.current.value;
@@ -21,22 +24,35 @@ const SearchHeader = memo(({ onSearch }) => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <a href="/">
+          <img className={styles.img} src="/images/logo.png" alt="logo" />
+        </a>
       </div>
-      <input
-        ref={inputRef}
-        className={styles.input}
-        type="search"
-        placeholder="Search..."
-        onKeyPress={onKeyPress}
-      />
-      <button className={styles.button} type="submit" onClick={onClick}>
-        <img
-          className={styles.buttonImg}
-          src="/images/search.png"
-          alt="search"
+      <div className={styles.searchsection}>
+        <input
+          ref={inputRef}
+          className={styles.input}
+          type="search"
+          placeholder="Search..."
+          onKeyPress={onKeyPress}
         />
-      </button>
+        <div className={styles.button} type="submit" onClick={onClick}>
+          <img
+            className={styles.buttonImg}
+            src="/images/search.png"
+            alt="search"
+          />
+        </div>
+      </div>
+      {isLoading ? <Skeleton type="top" />
+        :
+        <div className={styles.rightMenu}>
+          <VideoCallIcon />
+          <AppsIcon />
+          <NotificationsIcon />
+          <img></img>
+        </div>
+      }
     </header>
   );
 });
